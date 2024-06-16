b7180a1ec641f8c9340144df36a28dd6f28e4bca
Roman Krasovskiy <54819181+r-krasovskiy@users.noreply.github.com>
2024-06-16 19:46:49 +0300

    logic was improved

--- algo_task2_msg_decoding.py
+++ algo_task2_msg_decoding.py
@@ -1,10 +1,10 @@
 """
 Программа расшифровки ввода инструкций для марсохода.
-Номер попытки в Яндекс.Контент: 115240040.
+Номер попытки в Яндекс.Контент: 115280785.
 """
 import string
 
-DIGITS: str = string.digits
+DIGITS: set = set(string.digits)
 
 
 def read_message_recursive(message: str, index: int) -> tuple:
@@ -13,22 +13,20 @@ def read_message_recursive(message: str, index: int) -> tuple:
     Args:
      message - передаваемое марсходу зашифрованное сообщение.
      index - индекс символа сообщения.
-     DIGITS - строка, содержащая символы всех десятичных цифр
-      стандартной библиотеки string ('0123456789').
 
     Returns:
      кортеж с разобранной командой из квадратных скобок и индекс,
      который позволяет вернуться к символу сообщения после того,
      как содержимое квадратных скобок будет обработано.
     """
-    commands: list[int] = []
+    commands: list[str] = []
 
     while index < len(message):
         if message[index] == '[':
             index += 1
             inner_commands, index = read_message_recursive(message, index)
             commands.extend(inner_commands)
-        elif message[index] == '[':
+        elif message[index] == ']':
             return commands, index + 1
         elif message[index] in DIGITS:
             multiplier: str = ''
